83ce400929e8fc85d5eede4800a96ad73d02242a
Gabriel Aragao Nascimento Leal <gabriel2905836@aluno.ba.senac.br>
2026-08-03 21:17:06 -0300

    commit 01

---
+++ database/migrations/2026_08_03_231512_imc.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('imc', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome')->nullable();
+            $table->double('peso')->nullable();
+            $table->double('altura')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('imc');
+    }
+};
---
+++ resources/views/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Calcule seu IMC</h1>
+    <form method="post">
+        <P>Peso</P>
+        <input type="text" name="peso" placeholder="Digite sua peso">
+        <p>Altura</p>
+        <input type="text" name="altura" placeholder="Digite sua altura">
+
+        <div>
+            <button type="submit">Enviar</button>
+        </div>
+    </form>
+    <br><br>
+
+</body>
+
+</html>
